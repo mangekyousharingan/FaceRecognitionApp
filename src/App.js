59dd15ac220f4b423f718c27912818837f898c44
Sebastian Wdowiarz <swdowiarz@groupon.com>
2019-07-03 09:02:01 +0200

    Create basic page appearance

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import Particles from 'react-particles-js';
+import particlesOptions from './particles'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Particles className='particles' params={particlesOptions} />
+      <Navigation/>
+      <Logo/>
+      <Rank/>
+      <ImageLinkForm/>
+      {/*<ImageLinkForm></ImageLinkForm>*/}
+      {/*<FaceRecognition></FaceRecognition>*/}
     </div>
   );
 }
